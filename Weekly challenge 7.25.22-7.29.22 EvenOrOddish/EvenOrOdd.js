62f915f5e9d1f2c3e6c56c87ca4eb03b8da751c3
Kyle Stetz <kstetz3@live.com>
2022-07-27 11:30:01 -0400

    Updates and notes

--- Weekly challenge 7.25.22-7.29.22 EvenOrOddish/EvenOrOdd.js
+++ Weekly challenge 7.25.22-7.29.22 EvenOrOddish/EvenOrOdd.js
@@ -8,23 +8,23 @@ if result % 2 = 1 then oddish else evenish
  */
 let number = document.getElementById('num');// get number from input box
 console.log("Their number is: " + number);
-let btn = document.getElementById('evenOrOdd');
-btn.addEventListener('click',function(){evenOrOdd(number),false});
-function evenOrOdd(number){
-    let numArray = number.value;
+let btn = document.getElementById('evenOrOdd');// get reference to button
+btn.addEventListener('click',function(){evenOrOdd(number),false});// add an event listener for button
+function evenOrOdd(number){// create function to calculate even or oddish
+    let numArray = number.value;// get the number in an array
     console.log("I'm in the function");
     console.log("Your number is: " + number.value + " or is it " + number.valueAsNumber + " or is it " + numArray[0]);
-    let numCounter = 0;
-    let result;
-    for(let i = 0; i < numArray.length; i++){
-        numCounter = numCounter + parseInt(numArray[i],10);
+    let numCounter = 0;//set a counter to add all the numbers together
+    let result;//declare a variable to determine if numCounter is Evenish or Oddish
+    for(let i = 0; i < numArray.length; i++){// iterate through numArray
+        numCounter = numCounter + parseInt(numArray[i],10);// add each number together needed to type cast the array string to a number
         console.log("Current number total is: " + numCounter);
     }
-    if(numCounter % 2 == 0){
+    if(numCounter % 2 == 0){// determine if the input number is evenish or oddish
         result = "Evenish";
     }else{
         result = "Oddish";
     }
-    document.getElementById('result').innerHTML = "You number is " + result;
+    document.getElementById('result').innerHTML = "You number is " + result;// display the result on the HTML page
 
 }
