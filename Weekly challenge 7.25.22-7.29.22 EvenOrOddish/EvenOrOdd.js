1c11e8d3e02886fba0978016adb7040372a4434d
Kyle Stetz <kstetz3@live.com>
2022-07-27 11:40:06 -0400

    clean up making code more readable

--- Weekly challenge 7.25.22-7.29.22 EvenOrOddish/EvenOrOdd.js
+++ Weekly challenge 7.25.22-7.29.22 EvenOrOddish/EvenOrOdd.js
@@ -6,25 +6,25 @@ add them together
 if result % 2 = 1 then oddish else evenish
 
  */
-let number = document.getElementById('num');// get number from input box
-console.log("Their number is: " + number);
-let btn = document.getElementById('evenOrOdd');// get reference to button
-btn.addEventListener('click',function(){evenOrOdd(number),false});// add an event listener for button
-function evenOrOdd(number){// create function to calculate even or oddish
-    let numArray = number.value;// get the number in an array
-    console.log("I'm in the function");
-    console.log("Your number is: " + number.value + " or is it " + number.valueAsNumber + " or is it " + numArray[0]);
-    let numCounter = 0;//set a counter to add all the numbers together
-    let result;//declare a variable to determine if numCounter is Evenish or Oddish
-    for(let i = 0; i < numArray.length; i++){// iterate through numArray
-        numCounter = numCounter + parseInt(numArray[i],10);// add each number together needed to type cast the array string to a number
-        console.log("Current number total is: " + numCounter);
-    }
-    if(numCounter % 2 == 0){// determine if the input number is evenish or oddish
+let number = document.getElementById('num');                        // get number from input box
+let btn = document.getElementById('evenOrOdd');                     // get reference to button
+
+btn.addEventListener('click',function(){evenOrOdd(number),false});  // add an event listener for button
+                                                                    
+function evenOrOdd(number){                                         // create function to calculate even or oddish
+    let numArray = number.value;                                    // get the number in an array
+    let numCounter = 0;                                             //set a counter to add all the numbers together
+    let result;                                                     //declare a variable to determine if numCounter is Evenish or Oddish
+    for(let i = 0; i < numArray.length; i++){                       // iterate through numArray
+        numCounter = numCounter + parseInt(numArray[i],10);         // add each number together needed to type cast the array                                                            
+    }                                                               //from a string to a number using parseInt()
+
+
+    if(numCounter % 2 == 0){                                        // determine if the input number is evenish or oddish
         result = "Evenish";
     }else{
         result = "Oddish";
     }
-    document.getElementById('result').innerHTML = "You number is " + result;// display the result on the HTML page
+    document.getElementById('result').innerHTML = "You number is " + result;        // display the result on the HTML page
 
 }
